ee446e30d2073e78c09bef320a7590840379e34a
NguyenThiLanVy <lanvy342@gmail.com>
2024-08-31 23:42:51 +0700

    Add files via upload

--- lab01.py/b18.py
+++ lab01.py/b18.py
@@ -1,23 +1,42 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug 31 16:26:14 2024
+Created on Sat Aug 31 23:18:47 2024
 
 @author: USER
 """
-import datetime
 
-def cong_tru_gio_datetime(time1, time2, phep_toan="+"):
-  if phep_toan == "+":
-    return time1 + time2
-  else:
-    return time1 - time2
+h1 = int(input("Nhập giờ thứ nhất: "))
+m1 = int(input("Nhập giờ phút thứ nhất: "))
+s1 = int(input("Nhập giây thứ nhất: "))
 
-# Ví dụ sử dụng
-time1 = datetime.datetime(1900, 1, 1, 2, 30, 45)
-time2 = datetime.datetime(1900, 1, 1, 1, 15, 30)
+h2 = int(input("Nhập giờ thứ hai "))
+m2 = int(input("Nhập giờ phút thứ hai: "))
+s2 = int(input("Nhập giây thứ hai: "))
 
-ket_qua_cong = cong_tru_gio_datetime(time1, time2)
-ket_qua_tru = cong_tru_gio_datetime(time1, time2, "-")
+if h1 > 24 and h2 > 24 and m1 > 60 and m2 > 60 and s1 > 60 and s2 > 60:
+    print("thời gian không hợp lệ")
+else:
+    print("Thời gian")
+tonggiay1 = h1*3600 + m1*60 + s1
+tonggiay2 = h2*3600 + m2*60 + s2
 
-print("Kết quả cộng:", ket_qua_cong)
-print("Kết quả trừ:", ket_qua_tru)
+tong = tonggiay1 + tonggiay2
+sogio = tong//3600
+sophut = tong%3600 // 60
+sogiay = tong-(sogio*3600 + sophut*60)
+print("kết quả tổng: ", sogio, "h", sophut, "m", sogiay, "s")
+
+if tonggiay1 > tonggiay2:
+    hieu = tonggiay1 - tonggiay2
+    sogio = hieu//3600
+    sophut = hieu%3600//60
+    sogiay = hieu - (sogio*3600 + sophut*60)
+    print("Kết quả: ", sogio, "h", sophut, "m", sogiay, "s")
+    
+elif tonggiay1 < tonggiay2:
+    hieu = tonggiay1 - tonggiay2
+    sogio = hieu//3600
+    sophut = hieu%3600//60
+    sogiay = hieu - (sogio*3600 + sophut*60)
+    print("Kết quả: ", sogio, "h", sophut, "m", sogiay, "s")
+    
